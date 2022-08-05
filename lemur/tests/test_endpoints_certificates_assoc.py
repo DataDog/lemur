3de0417f8ee6f69e4e73a513da50ea0db6a039d2
Bob Shannon <bob.shannon@datadoghq.com>
2022-08-05 02:39:00 -0400

    Tweak

--- lemur/tests/test_endpoints_certificates_assoc.py
+++ lemur/tests/test_endpoints_certificates_assoc.py
@@ -80,8 +80,8 @@ def test_certificate_uniqueness():
     # Associate the same secondary certificate with the endpoint multiple times
     crt = _fake_cert()
 
-    # TODO(EDGE-1363) Expose API for managing secondary certificates associated with an endpoint
     for _ in range(0, 2):
+        # TODO(EDGE-1363) Expose API for managing secondary certificates associated with an endpoint
         endpoint.certificates_assoc.append(
             EndpointsCertificates(certificate=crt, endpoint=endpoint, primary_certificate=False)
         )
