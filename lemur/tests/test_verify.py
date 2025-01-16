f2aa5e5bf173f2b5e4095e82dea835af4479e503
Francesco Marabotto <francesco.marabotto@datadoghq.com>
2025-01-16 14:40:40 +0000

    test

--- lemur/tests/test_verify.py
+++ lemur/tests/test_verify.py
@@ -1,4 +1,4 @@
-# import pytest
+import pytest
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization, hashes
