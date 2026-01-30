e5cd46da2a17e58d06de178a4b8de087296a44e4
maperu <maxime.perusse@datadoghq.com>
2026-01-30 16:56:18 -0500

    Refactor token handling in authentication service
    - Convert user ID to string when creating JWT payload to ensure consistency.
    - Update user retrieval logic to handle string user IDs by converting them back to integers before fetching from the user service.
    - Adjust test vectors to reflect updated JWT tokens for valid users and admins.

--- lemur/auth/service.py
+++ lemur/auth/service.py
@@ -65,9 +65,9 @@ def create_token(user, aid=None, ttl=None):
 
     # Handle Just a User ID & User Object.
     if isinstance(user, int):
-        payload["sub"] = user
+        payload["sub"] = str(user)
     else:
-        payload["sub"] = user.id
+        payload["sub"] = str(user.id)
     if aid is not None:
         payload["aid"] = aid
     # Custom TTLs are only supported on Access Keys.
@@ -130,7 +130,10 @@ def login_required(f):
             if access_key.application_name:
                 g.caller_application = access_key.application_name
 
-        user = user_service.get(payload["sub"])
+        user_id = payload["sub"]
+        if isinstance(user_id, str):
+            user_id = int(user_id)
+        user = user_service.get(user_id)
 
         if not user.active:
             return dict(message="User is not currently active"), 403
