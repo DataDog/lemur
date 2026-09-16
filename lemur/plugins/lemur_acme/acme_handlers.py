4d841228b5effb27845cf100718f0fed8592a07e
Maxime Perusse <maxime.perusse@datadoghq.com>
2026-09-16 12:15:47 -0400

    Reject ambiguous ACME DNS providers

--- lemur/plugins/lemur_acme/acme_handlers.py
+++ lemur/plugins/lemur_acme/acme_handlers.py
@@ -576,6 +576,14 @@ class AcmeDnsHandler(AcmeHandler):
                     elif len(name) == match_length:
                         self.dns_providers_for_domain[domain].append(dns_provider)
 
+        dns_providers = self.dns_providers_for_domain[domain]
+        if len(dns_providers) > 1:
+            provider_names = ", ".join(sorted(x.name for x in dns_providers))
+            raise InvalidConfiguration(
+                "Multiple DNS providers match domain {}: {}. "
+                "Select a DNS provider explicitly.".format(domain, provider_names)
+            )
+
         return self.dns_providers_for_domain
 
     def finalize_authorizations(self, acme_client, authorizations):
